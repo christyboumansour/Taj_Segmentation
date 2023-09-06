0f640c9d9fb85cef15d4cf56c4ae6e001e1e152c
christyboumansour <114234859+christyboumansour@users.noreply.github.com>
2023-09-07 00:53:28 +0300

    Seg.py

--- Seg.py
+++ Seg.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 
-# Load your data
+# Loading data
 df = pd.read_csv('Sales1.csv')
 
-# Remove rows with a volume of 0
+# Removing rows with a volume of 0
 df = df[df['Volume'] != 0]
 
 # Group by 'Customer Name' and aggregate the data
@@ -28,11 +28,13 @@ st.title("Customer Segmentation App")
 
 logo_image= "https://tajgroupholding.com/cdn/shop/files/a5a99ea6-e35e-4c61-9d5c-d73392c89e40_160x160@2x.jpg?v=1670953223"
 st.sidebar.image(logo_image, width=75)
+
 # Create tabs
 tabs = ["RFM Clustering", "Purchasing Behavior Clustering"]
 tab_selected = st.sidebar.selectbox("Select Analysis:", tabs)
 
 if tab_selected == "RFM Clustering":
+    
     # Convert 'Invoice_Date' column to datetime format
     df['Invoice_Date'] = pd.to_datetime(df['Invoice_Date'])
 
@@ -65,42 +67,39 @@ if tab_selected == "RFM Clustering":
     # Define feature vector for RFM
     feature_vector_rfm = ['recency_log', 'frequency_log', 'amount_log']
 
-       # Apply K-means clustering for RFM analysis
+    # Apply K-means clustering for RFM analysis
     K_best_rfm = 5  # Based on your analysis
     model_rfm = KMeans(n_clusters=K_best_rfm, random_state=101)
-    model_rfm.fit(customer_history_df[feature_vector_rfm])
-    
-    # Create a dictionary to map original cluster labels to desired segment names
-    cluster_to_segment_mapping_rfm = {
-        0: "Regular Shoppers",
-        1: "Occasional Shoppers",
-        2: "High-Value and Frequency Shoppers",
-        3: "Balanced Shoppers",
-        4: "Moderate-Frequency High-Value Shoppers"
-    }
-    
-    # Map the original cluster labels to segment names
-    customer_history_df['Segment_rfm'] = model_rfm.labels_.map(cluster_to_segment_mapping_rfm)
-    
-    # Define consistent cluster colors based on the desired order
-    cluster_colors_rfm = {
-        "Regular Shoppers": 'green',
-        "Occasional Shoppers": 'yellow',
-        "High-Value and Frequency Shoppers": 'purple',
-        "Balanced Shoppers": 'blue',
-        "Moderate-Frequency High-Value Shoppers": 'red'
-    }
-    
-    # Create a consistent cluster_palette using the cluster_colors dictionary
-    cluster_palette_rfm = sns.color_palette([cluster_colors_rfm[label] for label in customer_history_df['Segment_rfm']])
-    
-        
-        # Map cluster labels to segment names for RFM
-    customer_history_df['Segment_rfm'] = customer_history_df['Cluster_rfm'].map(segment_labels_rfm)
-        
-        # Create a consistent cluster_palette using the cluster_colors dictionary
-    cluster_palette = sns.color_palette([cluster_colors[label] for label in segment_labels_rfm.values()])
+    model_rfm = model_rfm.fit(customer_history_df[feature_vector_rfm])
+    labels_rfm = model_rfm.labels_
 
+    # Add cluster labels to customer_history_df for RFM analysis
+    customer_history_df['Cluster_rfm'] = labels_rfm
+
+    # Define segment labels for RFM
+    segment_labels_rfm = cluster_labels = {
+    0: "Regular Shoppers",
+    1: "Occasional Shoppers",
+    2: "High-Value and Frequency Shoppers",
+    3: "Balanced Shoppers",
+    4: "Moderate-Frequency High-Value Shoppers"
+}
+
+    cluster_colors = {
+    0: 'blue',
+    1: 'green',
+    2: 'orange',
+    3: 'purple',
+    4: 'red'
+}
+
+    cluster_palette = sns.color_palette([cluster_colors[c] for c in segment_labels_rfm.keys()])
+    
+    # Map cluster labels to segment names for RFM
+    customer_history_df['Segment_rfm'] = customer_history_df['Cluster_rfm'].map(segment_labels_rfm)
+    
+
+    # Display RFM analysis results
     st.header("RFM Analysis Results")
 
  
@@ -156,10 +155,9 @@ These customers shop moderately often and have above-average spending habits."""
 
 
 
-    # ... (previous code) ...
 
 if tab_selected == "RFM Clustering":
-    # ... (previous RFM Clustering code) ...
+
 
     # Display a subset of the DataFrame with recency, frequency, and monetary values
     st.subheader("Subset of Customer Data")
@@ -189,14 +187,11 @@ if tab_selected == "RFM Clustering":
     else:
         st.write("No matching customers found.")
 
-# ... (rest of the code) ...
+
 
 
 elif tab_selected == "Purchasing Behavior Clustering":
-    # ... (previous Purchasing Behavior Clustering code) ...
-
-# ... (rest of the code) ...
-
+    
 
 
     # Select relevant features for segmentation
@@ -214,19 +209,12 @@ elif tab_selected == "Purchasing Behavior Clustering":
    
     model_pb.fit(X_scaled)
 
-    # Apply K-means clustering with the best K value
-      # Update with your desired number of clusters
-    
-    # labels_pb = model_pb.fit_predict(X_scaled)
-
-    # Add cluster labels to the DataFrame for Purchasing Behavior analysis
+ 
     df1['Cluster_pb'] = model_pb.fit_predict(X_scaled)
+    
     
     # Define cluster names for Purchasing Behavior
     
-
-
-
     # Specify the cluster names
     cluster_names = {
 0:  'Low-Value, Low-Volume Customers',
@@ -256,10 +244,8 @@ elif tab_selected == "Purchasing Behavior Clustering":
     df1['Cluster_Name'] = df1['Cluster_pb'].map(cluster_names)
     
    
-
-    # Your data loading and preprocessing code here
     
-    # Your clustering code here
+    
     import matplotlib.gridspec as gridspec
     # Create a figure and a GridSpec to arrange subplots
     fig = plt.figure(figsize=(15, 18))
@@ -334,5 +320,5 @@ Cluster 4: "Moderate-Value, Low-to-Moderate-Volume Customers" - Customers with m
     else:
         st.write("No matching customers found.")
 
-# ... (rest of the code) ...
+# ...  ...
 
